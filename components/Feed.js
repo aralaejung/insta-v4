9e5e22d15afa64c846db81f426286232a9b4736d
Aralaejung <aralaejung@gmail.com>
2022-06-26 14:45:53 +0700

    populate fake data using minifake and create feed story component

---
+++ components/Feed.js
@@ -0,0 +1,16 @@
+import Stories from "./Stories";
+export default function Feed() {
+  return (
+    <main>
+      <section>
+        {/* stories */}
+        <Stories />
+        {/* Posts */}
+      </section>
+      <section>
+        {/* mini profile */}
+        {/* Suggeciton */}
+      </section>
+    </main>
+  );
+}
